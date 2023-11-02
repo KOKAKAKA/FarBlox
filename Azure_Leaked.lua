590ee587cc18398b2833ddd1f75a8348b1c6af37
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2023-11-02 18:52:00 +0700

    Update Azure_Leaked.lua

--- Azure_Leaked.lua
+++ Azure_Leaked.lua
@@ -1,1 +1,105 @@
+function MakeScriptHub()
 loadstring(game:HttpGet(('https://pastebin.com/raw/wPsfrkwN'),true))()
+end
+local HWID=game:GetService("RbxAnalyticsService"):GetClientId()
+getgenv().Key = HWID
+getgenv().KeyInput = "string"
+
+local function readKey()
+  if readfile("/LimitXkey/key.txt") == getgenv().Key then
+    MakeScriptHub()
+    else
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Mobile%20Friendly%20Orion')))() 
+local Player = game.Players.LocalPlayer
+  local Window = OrionLib:MakeWindow({
+		Name = "Limit X Key System",
+		HidePremium = false,
+		SaveConfig = true,
+		ConfigFolder = "OrionTest",
+        IntroText = "Loading Script..."       
+}) 
+
+
+OrionLib:MakeNotification({
+	Name = "Logged In!",
+	Content = "You need key "..Player.Name..".",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+}) 
+
+
+local Tab = Window:MakeTab({
+	Name = "Key",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+}) 
+
+Tab:AddTextbox({
+	Name = "Key",
+	Default = "Enter Key.",
+	TextDisappear = true,
+	Callback = function(Value)
+		getgenv().KeyInput = Value
+	end	  
+}) 
+
+Tab:AddButton({
+    Name = "Check Key",
+    Callback = function()
+        if getgenv().KeyInput == getgenv().Key then
+            OrionLib:MakeNotification({
+                Name = "Checking Key",
+                Content = "Checking The Key You Entered",
+                Image = "rbxassetid://4483345998",
+                Time = 5
+            })
+            wait(2)
+            OrionLib:MakeNotification({
+                Name = "Correct Key!",
+                Content = "The key you entered is Correct.",
+                Image = "rbxassetid://4483345998",
+                Time = 5
+            })
+            wait(1)
+            OrionLib:Destroy()
+            wait(.3)
+            MakeScriptHub()
+          writefile("LimitXkey/key.txt", getgenv().Key)
+        else
+           OrionLib:MakeNotification({
+                Name = "Checking Key",
+                Content = "Checking The Key You Entered",
+                Image = "rbxassetid://4483345998",
+                Time = 5
+            })
+            wait(2)
+            OrionLib:MakeNotification({
+                Name = "Incorrect Key!",
+                Content = "The key you entered is incorrect.",
+                Image = "rbxassetid://4483345998",
+                Time = 5
+            })
+        end
+    end
+}) 
+
+Tab:AddButton({
+	Name = "Discord (Key)",
+	Callback = function()
+      		setclipboard("https://discord.com/invite/wWwFDPb6M7") 
+  	end    
+})
+  end
+end
+if isfolder("/LimitXkey") then
+  if isfile("/LimitXkey/key.txt", "") then
+    readKey()
+    else
+    writefile("/LimitXkey/key.txt", "")
+    readKey()
+  end
+  else
+  makefolder("/LimitXkey")
+  writefile("/LimitXkey/key.txt", "")
+  readKey()
+end
