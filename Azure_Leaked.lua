3fc15301c10bcbfa8574add2e3a6b14a04f861b1
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2023-11-08 13:16:54 +0700

    Update Azure_Leaked.lua

--- Azure_Leaked.lua
+++ Azure_Leaked.lua
@@ -11,7 +11,7 @@ end
 getgenv().Key = HWID
 getgenv().KeyInput = "string"
 local table = {
-	"0d786f66-6da2-4b9b-b90c-baae2a5c1992", 
+	"e6d44cc8-6295-4368-be3b-7e2e5bc6f070", 
 	"d5b2ef50-b909-4630-ab6a-8d4dfe60b950", 
 	"0aa71708-1234-47ab-8b03-81ee75c7c116", 
 	"858a7ae2-c06c-4849-b887-578bf32055df"
