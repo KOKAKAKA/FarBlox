1cd96c83714dadca582ac007e2dc2d7f83be86ba
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2023-11-27 13:29:50 +0700

    Update Azure_Leaked.lua

--- Azure_Leaked.lua
+++ Azure_Leaked.lua
@@ -1,1 +1,9 @@
+local KeyLibrary = KeyLibrary or loadstring(game:HttpGet('https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/setup_obf.lua'))()
+local KeySystem = KeyLibrary.new('LimitX')
+local Key = _G.key or nil
+if Key==nil or Key=="" then
+  return game.Players.LocalPlayer:Kick("Blacklisted.")
+elseif not KeySystem:verifyPremiumKey(Key) then
+  return game.Players.LocalPlayer:Kick("Blacklisted.")
+end
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/KOKAKAKA/FarBlox/main/PaidLimit'),true))()
